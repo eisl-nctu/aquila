55423d70e78acd6fa4bc9a3ca18cd7b04420b805
Daichou <tommy0705c@gmail.com>
2020-02-12 15:32:40 +0800

    small core verilate environment init

---
+++ verilate/core-ver-src/aquila_testharness.sv
@@ -0,0 +1,128 @@
+
+`timescale 1 ns / 1 ps
+// =============================================================================
+//  Program : aquila.v
+//  Author  : Hon-Chou Dai (Daichou)
+//  Date    : FEB/12/2020
+// -----------------------------------------------------------------------------
+//  Revision information:
+//
+//  NONE.
+// -----------------------------------------------------------------------------
+//  Description:
+//  This is the top-level Aquila SoC testharness module.
+// -----------------------------------------------------------------------------
+//  License information:
+//
+//  This software is released under the BSD-3-Clause Licence,
+//  see https://opensource.org/licenses/BSD-3-Clause for details.
+//  In the following license statements, "software" refers to the
+//  "source code" of the complete hardware/software system.
+//
+//  Copyright 2019,
+//                    Embedded Intelligent Systems Lab (EISL)
+//                    Deparment of Computer Science
+//                    National Chiao Tung Uniersity
+//                    Hsinchu, Taiwan.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+// =============================================================================
+
+module aquila_testharness #
+(
+    // Parameters of Axi Slave Bus Interface S_CONFIG_PORT
+    parameter integer C_S_CONFIG_PORT_DATA_WIDTH    = 32,
+    //parameter integer C_S_CONFIG_PORT_ADDR_WIDTH    = 5,
+    // Parameters of Axi Master Bus Interface M_ICACHE_PORT
+    parameter integer C_M_ICACHE_PORT_ID_WIDTH		= 1,
+    parameter integer C_M_ICACHE_PORT_ADDR_WIDTH	= 32,
+    parameter integer C_M_ICACHE_PORT_DATA_WIDTH	= 32,
+
+    // Parameters of Axi Master Bus Interface M_DCACHE_PORT
+    parameter integer C_M_DCACHE_PORT_ID_WIDTH		= 1,
+    parameter integer C_M_DCACHE_PORT_ADDR_WIDTH	= 32,
+    parameter integer C_M_DCACHE_PORT_DATA_WIDTH	= 32,
+
+    // Parameters of Axi Master Bus Interface M_DEVICE_PORT
+    parameter integer C_M_DEVICE_PORT_ADDR_WIDTH    = 32,
+    parameter integer C_M_DEVICE_PORT_DATA_WIDTH    = 32
+)
+(
+  input logic   clk,
+  input logic   rst_n,
+  input logic   [C_S_CONFIG_PORT_DATA_WIDTH - 1 : 0] main_memory_addr
+)
+
+
+// Declaration of local signals.
+wire                                      RISCV_rst;
+
+wire                                      M_ICACHE_strobe, M_ICACHE_done;
+wire                                      M_DCACHE_strobe, M_DCACHE_done;
+wire                                      M_DCACHE_rw;
+wire [C_S_CONFIG_PORT_DATA_WIDTH - 1 : 0] M_ICACHE_addr, M_DCACHE_addr;
+wire [255 : 0] M_ICACHE_datain, M_DCACHE_datain, M_DCACHE_dataout;
+
+wire                                      M_DEVICE_strobe;
+wire [C_S_CONFIG_PORT_DATA_WIDTH - 1 : 0] M_DEVICE_addr;
+wire                                      M_DEVICE_rw;
+wire [C_S_CONFIG_PORT_DATA_WIDTH/8-1 : 0] M_DEVICE_byte_enable;
+wire [C_S_CONFIG_PORT_DATA_WIDTH - 1 : 0] M_DEVICE_core2dev_data;
+wire                                      M_DEVICE_data_ready;
+wire [C_S_CONFIG_PORT_DATA_WIDTH - 1 : 0] M_DEVICE_dev2core_data;
+
+always_comb
+  RISCV_rst = rst_n;
+
+// Instiantiation of the top-level Aquila core module.
+aquila_top aquila_core(
+    .clk(clk),
+    .rst(RISCV_rst),
+    .base_addr(main_memory_addr),
+
+    .M_ICACHE_strobe(M_ICACHE_strobe),
+    .M_ICACHE_addr(M_ICACHE_addr),
+    .M_ICACHE_done(M_ICACHE_done),
+    .M_ICACHE_datain(M_ICACHE_datain),
+
+    .M_DCACHE_strobe(M_DCACHE_strobe),
+    .M_DCACHE_addr(M_DCACHE_addr),
+    .M_DCACHE_rw(M_DCACHE_rw),
+    .M_DCACHE_dataout(M_DCACHE_dataout),
+    .M_DCACHE_done(M_DCACHE_done),
+    .M_DCACHE_datain(M_DCACHE_datain),
+
+    .M_DEVICE_strobe(M_DEVICE_strobe),
+    .M_DEVICE_addr(M_DEVICE_addr),
+    .M_DEVICE_rw(M_DEVICE_rw),
+    .M_DEVICE_byte_enable(M_DEVICE_byte_enable),
+    .M_DEVICE_core2dev_data(M_DEVICE_core2dev_data),
+    .M_DEVICE_data_ready(M_DEVICE_data_ready),
+    .M_DEVICE_dev2core_data(M_DEVICE_dev2core_data)
+);
---
+++ verilate/core-ver-src/dp_ram.sv
@@ -0,0 +1,197 @@
+
+module dp_ram
+  #(
+    parameter int ADDR_WIDTH = 32,
+    parameter int DATA_WIDTH = 32,
+    parameter int MEM_SIZE = 32'h200000, //small memory for verilator
+    parameter int ACCESS_LANTENCY = 8'h0 // done signal control
+  )(
+    // Clock and Reset
+    input  logic                      clk,
+
+    input  logic                      strobe_icache
+    input  logic [ADDR_WIDTH-1:0]     addr_icache_i,
+    output logic [DATA_WIDTH-1:0]     rdata_icache_o,
+    output logic                      done_icache_o,
+
+    input  logic                      strobe_dcache,
+    input  logic [ADDR_WIDTH-1:0]     addr_dcache_i,
+    input  logic [DATA_WIDTH-1:0]     wdata_dcache_i,
+    output logic [DATA_WIDTH-1:0]     rdata_dcache_o,
+    input  logic                      rw_dcache_i,
+    output logic                      done_dcache_o
+  );
+
+  typedef enum logic [1:0] {IDLE = 2'b0, READ, WRITE, DONE} state_t;
+
+  function integer clogb2 (input integer bit_depth);
+  begin
+  for(clogb2=0; bit_depth>0; clogb2=clogb2+1)
+    bit_depth = bit_depth >> 1;
+  end
+  endfunction
+
+  localparam PART_ADDR_WIDTH = clogb2(MEM_SIZE);
+
+  state_t icache_cur_state, icache_next_state;
+  state_t dcache_cur_state, dcache_next_state;
+
+  logic [PART_ADDR_WIDTH-1:0] icache_addr;
+  logic [PART_ADDR_WIDTH-1:0] dcache_addr;
+  logic [7:0] mem [MEM_SIZE-1:0];
+  logic [7:0] icache_delay_counter;
+  logic [7:0] dcache_delay_counter;
+  logic icache_reads_done;
+  logic dcache_reads_done;
+  logic dcache_writes_done;
+
+  always_comb
+    icache_addr = addr_icache_i[PART_ADDR_WIDTH-1:0];
+  always_comb
+    dcache_addr = addr_dcache_i[PART_ADDR_WIDTH-1:0];
+
+  always_ff @(posedge clk) begin
+    if (icache_cur_state == IDLE || icache_cur_state == DONE) icache_delay_counter <= 0;
+    else icache_delay_counter <= icache_delay_counter + 1;
+  end
+
+  always_ff @(posedge clk) begin
+    if (dcache_cur_state == IDLE || dcache_cur_state == DONE) dcache_delay_counter <= 0;
+    else dcache_delay_counter <= dcache_delay_counter + 1;
+  end
+
+  always_comb
+    icache_reads_done = icache_delay_counter >= ACCESS_LANTENCY;
+
+  always_comb
+    dcache_reads_done = dcache_delay_counter >= ACCESS_LANTENCY;
+  always_comb
+    dcache_writes_done = dcache_delay_counter >= ACCESS_LANTENCY;
+
+  always_ff @(posedge clk) begin
+    if (~rst_n)
+      icache_cur_state <= IDLE;
+    else
+      icache_cur_state <= icache_next_state;
+  end
+
+  always_ff @(posedge clk) begin
+    if (~rst_n)
+      dcache_cur_state <= IDLE;
+    else
+      dcache_cur_state <= dcache_next_state;
+  end
+
+  always_comb begin
+    case (icache_cur_state)
+      IDLE: if (strobe_icache) icache_next_state = READ;
+            else icache_next_state = IDLE;
+      READ: if (icache_reads_done) icache_next_state = DONE;
+            else icache_next_state = READ;
+      DONE: icache_next_state = IDLE;
+      default: icache_next_state = IDLE;
+    endcase
+  end
+
+  always_comb begin
+    case (dcache_cur_state)
+      IDLE: if (strobe_dcache) dcache_next_state = (rw_dcache_i ?) WRITE : READ;
+            else dcache_next_state = IDLE;
+      READ: if (dcache_reads_done) dcache_next_state = DONE;
+            else dcache_next_state = READ;
+      WRITE:if (dcache_writes_done) dcache_next_state = DONE;
+            else dcache_next_state = WRITE;
+      DONE: dcache_next_state = IDLE;
+      default: dcache_next_state = IDLE;
+    endcase
+  end
+
+  always_ff @(posedge clk) begin
+    if (dcache_cur_state == WRITE) begin
+      mem[dcache_addr] <=   wdata_dcache_i[7:0];
+      mem[dcache_addr+1] <= wdata_dcache_i[15:8];
+      mem[dcache_addr+2] <= wdata_dcache_i[23:16];
+      mem[dcache_addr+3] <= wdata_dcache_i[31:24];
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (dcache_cur_state == READ) begin
+      rdata_dcache_o <= {mem[dcache_addr+3],mem[dcache_addr+2],mem[dcache_addr+1],mem[dcache_addr]};
+    end else begin
+      rdata_dcache_o <= 0;
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (icache_cur_state == READ) begin
+      rdata_icache_o <= {mem[icache_addr+3],mem[icache_addr+2],mem[icache_addr+1],mem[icache_addr]};
+    end else begin
+      rdata_icache_o <= 0;
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (icache_cur_state == DONE)
+      done_icache_o <= 1;
+    else
+      done_icache_o <= 0;
+  end
+
+  always_ff @(posedge clk) begin
+    if (dcache_cur_state == DONE)
+      done_dcache_o <= 1;
+    else
+      done_dcache_o <= 0;
+  end
+
+  function [31:0] readWord;
+    /* verilator public function method*/
+    input integer byte_addr;
+    begin
+      if (byte_addr < MEM_SIZE)
+        readWord = {mem[byte_addr[PART_ADDR_WIDTH-1:0]+3],
+                    mem[byte_addr[PART_ADDR_WIDTH-1:0]+2],
+                    mem[byte_addr[PART_ADDR_WIDTH-1:0]+1],
+                    mem[byte_addr[PART_ADDR_WIDTH-1:0]]};
+      else
+        readWord = 32'hdeadbeef;
+    end
+  endfunction
+
+  task writeWord;
+    /* verilator public function method*/
+    input integer byte_addr;
+    input [31:0] val;
+    begin
+      if (byte_addr < MEM_SIZE) begin
+        mem[byte_addr[PART_ADDR_WIDTH-1:0]+3] = val[31:24];
+        mem[byte_addr[PART_ADDR_WIDTH-1:0]+2] = val[23:16];
+        mem[byte_addr[PART_ADDR_WIDTH-1:0]+1] = val[15:8];
+        mem[byte_addr[PART_ADDR_WIDTH-1:0]] = val[7:0];
+      end
+    end
+  endtask
+
+  task writeByte;
+    /* verilator public function method*/
+    input integer byte_addr;
+    input [7:0] val;
+    begin
+      if (byte_addr < MEM_SIZE) begin
+        mem[byte_addr[PART_ADDR_WIDTH-1:0]] = val;
+      end
+    end
+  endtask
+
+  function [7:0] readByte;
+    /* verilator public function method*/
+    input integer byte_addr;
+    begin
+      if (byte_addr < MEM_SIZE)
+        readWord = mem[byte_addr[PART_ADDR_WIDTH-1:0];
+      else
+        readWord = 8'hef; //dummy
+    end
+  endfunction
+endmodule
