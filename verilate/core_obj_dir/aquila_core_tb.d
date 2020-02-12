9e627b7d1fe12f09344c1471ffa8b5972fda3cec
Daichou <tommy0705c@gmail.com>
2020-02-12 23:19:37 +0800

    fix aquila_testharness bad rst_n typo

--- verilate/core_obj_dir/aquila_core_tb.d
+++
@@ -1,8 +0,0 @@
-aquila_core_tb.o: ../core-ver-src/aquila_core_tb.cpp \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated.h Vaquila_testharness.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- Vaquila_testharness__Dpi.h \
- /usr/local/share/verilator/include/vltstd/svdpi.h \
- /usr/local/share/verilator/include/verilated.h
