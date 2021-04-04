da1a5410686d9823df3d61c4a6a845ef24784c2d
eisl-nctu <58967501+eisl-nctu@users.noreply.github.com>
2021-04-04 11:52:18 +0800

    Update string copy & comparison functions.
    Replace strcpy(), strncpy(), strncmp(), and strcmp() with code extracted from the RISC-V port of Newlib at https://github.com/riscv/riscv-newlib.
    These functions boost DMIPS by 28.5%.

--- sw/elibc/string.c
+++ sw/elibc/string.c
@@ -8,7 +8,10 @@
 // -----------------------------------------------------------------------------
 //  Revision information:
 //
-//  None.
+//  Oct/6/2020, by Chun-Jen Tsai:
+//    Replace strcpy(), strncpy(), strncmp(), and strcmp() with code extracted
+//    from the RISC-V port of Newlib at https://github.com/riscv/riscv-newlib.
+//    These functions boost DMIPS by 28.5%.
 // -----------------------------------------------------------------------------
 //  License information:
 //
@@ -54,29 +57,191 @@
 #include <stdio.h>
 #include <string.h>
 
+// ------------------------------------------------------------------------------
+//  Functions extracted from Newlib.
+//
+#define UNALIGNED(X,Y) (((long)X & (sizeof(long)-1))|((long)Y & (sizeof(long)-1)))
+#define DETECTNULL(X) (((X)-0x01010101) & ~(X) & 0x80808080)
+
+char *strcpy(char *dst, char *src)
+{
+    char *dst0 = dst;
+    const char *src0 = src;
+    long *aligned_dst;
+    const long *aligned_src;
+
+    // If SRC or DEST is unaligned, then copy bytes.
+    if (!UNALIGNED(src0, dst0))
+    {
+        aligned_dst = (long *) dst0;
+        aligned_src = (long *) src0;
+
+        // Perform word copying since both strings are word-aligned.
+        while (!DETECTNULL(*aligned_src))
+        {
+            *aligned_dst++ = *aligned_src++;
+        }
+
+        dst0 = (char *) aligned_dst;
+        src0 = (char *) aligned_src;
+    }
+
+    // Copying trailing bytes for aligned strings, or the entire
+    // strings if not aligned.
+    while ((*dst0++ = *src0++)) ;
+
+    return dst;
+}
+
+int strcmp(char *s1, char *s2)
+{
+    unsigned long *a1;
+    unsigned long *a2;
+
+    /* If s1 or s2 are unaligned, then compare bytes. */
+    if (!UNALIGNED(s1, s2))
+    {
+        /* If s1 and s2 are word-aligned, compare them a word at a time. */
+        a1 = (unsigned long *) s1;
+        a2 = (unsigned long *) s2;
+        while (*a1 == *a2)
+        {
+            /* To get here, *a1 == *a2, thus if we find a null in *a1,
+               then the strings must be equal, so return zero.  */
+            if (DETECTNULL(*a1))
+                return 0;
+
+            a1++;
+            a2++;
+        }
+
+        /* A difference was detected in last few bytes of s1, so search bytewise */
+        s1 = (char *) a1;
+        s2 = (char *) a2;
+    }
+
+    while (*s1 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (*(unsigned char *) s1) - (*(unsigned char *) s2);
+}
+
+char *strncpy(char *dst, char *src, size_t n)
+{
+    char *dst0 = dst;
+    const char *src0 = src;
+    long *aligned_dst;
+    const long *aligned_src;
+
+    // If SRC or DEST is unaligned, then copy bytes.
+    if (!UNALIGNED(src0, dst0))
+    {
+        aligned_dst = (long *) dst0;
+        aligned_src = (long *) src0;
+
+        // Perform word copying since both strings are word-aligned.
+        while (!DETECTNULL(*aligned_src) && n > sizeof(long))
+        {
+            *aligned_dst++ = *aligned_src++;
+            n -= sizeof(long);
+        }
+
+        dst0 = (char *) aligned_dst;
+        src0 = (char *) aligned_src;
+    }
+
+    // Copying trailing bytes for aligned strings, or the entire
+    // strings if not aligned.
+    while ((*dst0++ = *src0++) && n-- > 0) ;
+    if (++n == 0) *dst0 = 0;
+
+    return dst;
+}
+
+int strncmp(char *s1, char *s2, size_t n)
+{
+    unsigned long *a1;
+    unsigned long *a2;
+
+    /* If s1 or s2 are unaligned, then compare bytes. */
+    if (!UNALIGNED(s1, s2))
+    {
+        /* If s1 and s2 are word-aligned, compare them a word at a time. */
+        a1 = (unsigned long *) s1;
+        a2 = (unsigned long *) s2;
+        while (*a1 == *a2 && n > sizeof(long))
+        {
+            /* To get here, *a1 == *a2, thus if we find a null in *a1,
+               then the strings must be equal, so return zero.  */
+            if (DETECTNULL(*a1))
+                return 0;
+
+            a1++;
+            a2++;
+            n -= sizeof(long);
+        }
+
+        /* A difference was detected in last few bytes of s1, so search bytewise */
+        s1 = (char *) a1;
+        s2 = (char *) a2;
+    }
+
+    while (n && *s1 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+        n--;
+    }
+    return (*(unsigned char *) s1) - (*(unsigned char *) s2);
+}
+
 void *memcpy(void *d, void *s, size_t n)
 {
-    unsigned char *dst = (unsigned char *) d;
-    unsigned char *src = (unsigned char *) s;
+    char *d0 = d;
+    const char *s0 = s;
+    long *aligned_d;
+    const long *aligned_s;
 
-    for (int idx = 0; idx < n; idx++) *(dst++) = *(src++);
+    // If SRC or DEST is unaligned, then copy bytes.
+    if (!UNALIGNED(s0, d0))
+    {
+        aligned_d = (long *) d0;
+        aligned_s = (long *) s0;
+
+        // Perform word copying since both pointers are word-aligned.
+        while (!DETECTNULL(*aligned_s) && n > sizeof(long))
+        {
+            *aligned_d++ = *aligned_s++;
+            n -= sizeof(long);
+        }
+
+        d0 = (char *) aligned_d;
+        s0 = (char *) aligned_s;
+    }
+
+    // Copying trailing bytes for aligned memory block, or the entire
+    // memory block if not aligned.
+    while ((*d0++ = *s0++) && n-- > 0) ;
+    if (++n == 0) *d0 = 0;
+
     return d;
 }
 
+//  End of functions extracted from Newlib.
+// ------------------------------------------------------------------------------
+
 void *memmove(void *d, void *s, size_t n)
 {
-    unsigned char *dst = (unsigned char *) d;
-    unsigned char *src = (unsigned char *) s;
+    unsigned char *dst = (unsigned char *) d + n - 1;
+    unsigned char *src = (unsigned char *) s + n - 1;
 
-    if ((unsigned) d >= (unsigned) s &&
-        (unsigned) d <= (unsigned) s + n)
+    if ((unsigned) d >= (unsigned) s && (unsigned) d <= (unsigned) s + n)
     {
-        for (int idx = n - 1; idx >= 0; idx--) dst[idx] = src[idx];
+        while (n--) *(dst--) = *(src--);
     }
-    else
-    {
-        for (int idx = 0; idx < n; idx++) *(dst++) = *(src++);
-    }
+    else memcpy(d, s, n);
 
     return d;
 }
@@ -85,7 +250,7 @@ void *memset(void *d, int v, size_t n)
 {
     unsigned char *dst = (unsigned char *) d;
 
-    for (int idx = 0; idx < n; idx++) *(dst++) = (unsigned char) v;
+    while (n--) *(dst++) = (unsigned char) v;
     return d;
 }
 
@@ -95,24 +260,6 @@ long strlen(char *s)
 
     while (*s++) n++;
     return n;
-}
-
-char *strcpy(char *dst, char *src)
-{
-    char *tmp = dst;
-
-    while (*src) *(tmp++) = *(src++);
-    *tmp = 0;
-    return dst;
-}
-
-char *strncpy(char *dst, char *src, size_t n)
-{
-    char *tmp = dst;
-
-    while (*src && n) *(tmp++) = *(src++), n--;
-    while (n--) *(tmp++) = 0;
-    return dst;
 }
 
 char *strcat(char *dst, char *src)
@@ -133,56 +280,4 @@ char *strncat(char *dst, char *src, size_t n)
     while (*src && n) *(tmp++) = *(src++), n--;
     *tmp = 0;
     return dst;
-}
-
-int  strcmp(char *s1, char *s2)
-{
-    int value;
- 
-    s1--, s2--;
-    do
-    {
-        s1++, s2++;
-        if (*s1 == *s2)
-        {
-            value = 0;
-        }
-        else if (*s1 < *s2)
-        {
-            value = -1;
-            break;
-        }
-        else
-        {
-            value = 1;
-            break;
-        }
-    } while (*s1 != 0 && *s2 != 0);
-    return value;
-}
-
-int  strncmp(char *s1, char *s2, size_t n)
-{
-    int value;
-
-    s1--, s2--;
-    do
-    {
-        s1++, s2++;
-        if (*s1 == *s2)
-        {
-            value = 0;
-        }
-        else if (*s1 < *s2)
-        {
-            value = -1;
-            break;
-        }
-        else
-        {
-            value = 1;
-            break;
-        }
-    } while (--n && *s1 != 0 && *s2 != 0);
-    return value;
 }
